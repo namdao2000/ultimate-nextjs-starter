af54167d97b2bc1328a6961c6339680aaa0a73b8
Nam <namdao2000@gmail.com>
2022-08-25 14:44:56 -0700

    feat: fixed build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
-import { customTheme } from './_customTheme';
+import { customTheme } from '../styles/customTheme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
