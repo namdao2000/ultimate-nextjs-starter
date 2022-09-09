429705fce6910ae002a8987e19dde27870222a4e
namdao2000 <namdao2000@gmail.com>
2022-09-09 11:38:13 -0700

    feat: improved logging

--- src/lib/utils/logger.ts
+++ src/lib/utils/logger.ts
@@ -1,3 +1,21 @@
 import pino from 'pino';
 
-export const logger = pino();
+// Configured logging to work with datadog in production
+export const logger = pino({
+  formatters: {
+    level(level) {
+      return process.env.NODE_ENV === 'development'
+        ? { level }
+        : { status: level };
+    },
+  },
+  serializers: {
+    error: pino.stdSerializers.err,
+  },
+  transport:
+    process.env.NODE_ENV === 'development'
+      ? {
+          target: 'pino-pretty',
+        }
+      : undefined,
+});
